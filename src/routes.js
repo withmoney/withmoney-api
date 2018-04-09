11182d061cbbb6da9637d67f5298cd42f825b886
David Costa <davidcostadev@gmail.com>
2018-04-09 20:44:55 -0300

    Eslint in file

--- src/routes.js
+++ src/routes.js
@@ -3,10 +3,9 @@ import * as Users from './controllers/Users';
 
 const router = Router();
 
-
 router.get('/', (req, res) => {
   res.send('legal');
-})
+});
 
 router.get('/api/v1/users', Users.list);
 
