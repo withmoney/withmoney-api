a079b3ad1ad97a62c24de9b1da9c6c0f6296d221
David Costa <davidcostadev@gmail.com>
2018-04-09 20:44:50 -0300

    Add port in a var

--- src/server.js
+++ src/server.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import routes from './routes';
-import db from './models'
 
 const app = express();
 
 app.use(routes);
 
-app.listen(process.env.NODE_PORT || 3000, () => {
-  console.log('server on');
+const port = process.env.NODE_PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`server on: ${port}`);
 });
