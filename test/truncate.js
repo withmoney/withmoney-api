496c1f9dc7e57d2bc360160223d10414ba603d49
David Costa <david.costa@codeminer42.com>
2018-04-16 15:16:38 -0300

    Add trucate

---
+++ test/truncate.js
@@ -0,0 +1,13 @@
+import models from '../src/models';
+
+async function truncate() {
+  const modelsList = await Promise.all(Object.keys(models).map((key) => {
+    if (['sequelize', 'Sequelize'].includes(key)) return null;
+
+    return models[key].destroy({ where: {}, forse: true });
+  }));
+
+  return modelsList;
+}
+
+export default truncate;
