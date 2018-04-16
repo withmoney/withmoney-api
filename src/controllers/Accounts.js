b7a0f7730b40a5165c552ea7a2b0b739523bf305
David Costa <davidcostadev@gmail.com>
2018-04-16 07:07:29 -0300

    add route accounts

---
+++ src/controllers/Accounts.js
@@ -0,0 +1,140 @@
+import paginationParse from '../utils/pagination';
+import database, { Accounts, Users } from '../models';
+
+export const list = async ({ query }, res) => {
+  const limit = parseInt(query.limit, 10) || 100;
+  const page = parseInt(query.page, 10) || 1;
+  const name = query.name || '';
+  const batch = query.batch || null;
+
+  const select = {
+    limit,
+    offset: parseInt(limit, 10) * (page - 1),
+    order: [['id', 'DESC']],
+  };
+  const where = {};
+
+  if (name.length) {
+    where.name = name;
+  }
+
+  if (batch) {
+    let models = batch.split(',');
+
+    if (models.length) {
+      models = models.map(model => ({
+        model: database[model],
+      }));
+      select.include = models;
+    }
+  }
+
+  try {
+    let data = await Accounts.findAll(select);
+    const { count } = await Accounts.findAndCountAll({ where });
+
+    data = data.map(a => a.toJSON());
+
+    const pagination = paginationParse(count, page, limit);
+
+    res.json({
+      data,
+      pagination,
+    });
+  } catch (e) {
+    console.error(e);
+    res.status(500).send(e);
+  }
+};
+
+export const create = async (req, res) => {
+  const {
+    name,
+    userId,
+    type,
+    initalValue,
+  } = req.body;
+
+  try {
+    const entity = await Accounts.create({
+      name,
+      userId,
+      type,
+      initalValue,
+    });
+
+    res.json(entity);
+  } catch (e) {
+    console.error(e);
+    res.status(500).send(e);
+  }
+};
+
+export const get = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const entity = await Accounts.findById(id);
+
+    res.json(entity);
+  } catch (e) {
+    console.error(e);
+    res.status(500).send(e);
+  }
+};
+
+export const update = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const entity = await Accounts.findById(id);
+
+    if (req.body.userId) {
+      req.body.userId = parseInt(req.body.userId, 10);
+    }
+    if (req.body.initalValue) {
+      req.body.initalValue = parseFloat(req.body.initalValue);
+    }
+
+    const data = await entity.update(req.body);
+
+    res.json(data.toJSON());
+  } catch (e) {
+    console.error(e);
+    res.status(500).send(e);
+  }
+};
+
+export const accounts = async (req, res) => {
+  const { id } = req.params;
+
+  const select = {
+    include: [
+      Accounts,
+    ],
+  };
+
+  try {
+    const user = await Users.findById(id, select);
+
+    res.json(user);
+  } catch (e) {
+    console.error(e);
+    res.status(500).send(e);
+  }
+};
+
+export const destroy = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    await Accounts.destroy({
+      where: { id },
+    });
+
+    res.status(204).send();
+  } catch (e) {
+    console.error(e);
+    res.status(500).send(e);
+  }
+};
--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,27 @@
 import { Router } from 'express';
 import * as Users from './controllers/Users';
+import * as Accounts from './controllers/Accounts';
 
 const router = Router();
 
 router.get('/', (req, res) => {
-  res.send('legal');
+  res.send({
+    apis: {
+      v1: {
+        users: [
+          '[get] /api/v1/users',
+          '[post] /api/v1/users',
+          '[get] /api/v1/users/:id',
+          '[delete] /api/v1/users/:id',
+          '[put] /api/v1/users/:id',
+          '[get] /api/v1/users/:id/accounts',
+        ],
+      },
+    },
+  });
 });
+
+// users
 
 router.get('/api/v1/users', Users.list);
 router.post('/api/v1/users', Users.create);
@@ -13,6 +29,18 @@ router.get('/api/v1/users/:id', Users.get);
 router.delete('/api/v1/users/:id', Users.destroy);
 router.put('/api/v1/users/:id', Users.update);
 
+// users associated
+
 router.get('/api/v1/users/:id/accounts', Users.accounts);
+
+// accounts
+
+router.get('/api/v1/accounts', Accounts.list);
+router.post('/api/v1/accounts', Accounts.create);
+router.get('/api/v1/accounts/:id', Accounts.get);
+router.delete('/api/v1/accounts/:id', Accounts.destroy);
+router.put('/api/v1/accounts/:id', Accounts.update);
+
+// router.get('/api/v1/users/:id/accounts', Accounts.accounts);
 
 export default router;
