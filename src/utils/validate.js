00650c786accef1956fba67fa342f24b02cb03fd
David Costa <david.costa@codeminer42.com>
2018-04-25 17:23:34 -0300

    Add create from controller

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -3,3 +3,5 @@ import R from 'ramda';
 export const string = R.compose(Boolean, R.length);
 
 export const number = num => Number.isInteger(parseInt(num, 10));
+
+export const float = num => !Number.isNaN(parseFloat(num));
