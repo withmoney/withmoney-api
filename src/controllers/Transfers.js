2f1120badcd6c660a7bb11c02a8aa4106075a437
David Costa <david.costa@codeminer42.com>
2018-04-25 11:47:18 -0300

    Add Create transfers

--- src/controllers/Transfers.js
+++ src/controllers/Transfers.js
@@ -3,4 +3,25 @@ import * as Controller from './Controller';
 
 export const list = (req, res) => Controller.list(req, res, Transfers);
 
-export const a = true;
+export const create = async (req, res) => {
+  const {
+    value,
+    accountFromId,
+    accountToId,
+    transferDate,
+  } = req.body;
+
+  try {
+    const entity = await Transfers.create({
+      value,
+      accountFromId,
+      accountToId,
+      transferDate,
+    });
+
+    res.json(entity);
+  } catch (e) {
+    console.error(e);
+    res.status(500).send(e);
+  }
+};
