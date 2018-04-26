d24d224b58be72ab3d9ffdd7690bf6c97a372aa5
David Costa <david.costa@codeminer42.com>
2018-04-26 16:44:47 -0300

    Apply update on Users Transfers and Transactions

--- src/controllers/Transfers.js
+++ src/controllers/Transfers.js
@@ -18,34 +18,11 @@ export const create = (req, res) => Controller.create(req, res, Transfers, {
 
 export const get = async (req, res) => Controller.get(req, res, Transfers);
 
-export const update = async (req, res) => {
-  const { id } = req.params;
-
-  try {
-    const entity = await Transfers.findById(id);
-
-    if (req.body.accountFromId) {
-      req.body.accountFromId = parseInt(req.body.accountFromId, 10);
-    }
-    if (req.body.accountToId) {
-      req.body.accountToId = parseInt(req.body.accountToId, 10);
-    }
-    if (req.body.value) {
-      req.body.value = parseFloat(req.body.value);
-    }
-    if (req.body.transferDate) {
-      req.body.transferDate = req.body.transferDate;
-    }
-
-    await entity.update(req.body);
-
-    const transferUpdated = await Transfers.findById(id);
-
-    res.json(transferUpdated);
-  } catch (e) {
-    console.error(e);
-    res.status(500).send(e);
-  }
-};
+export const update = (req, res) => Controller.update(req, res, Transfers, {
+  value: valueSelType,
+  accountFromId: accountFromIdSelType,
+  accountToId: accountToIdSelType,
+  transferDate: transferDateSelType,
+});
 
 export const destroy = (req, res) => Controller.destroy(req, res, Transfers);
--- src/controllers/Users.js
+++ src/controllers/Users.js
@@ -10,25 +10,9 @@ export const create = (req, res) => Controller.create(req, res, Users, {
 
 export const get = async (req, res) => Controller.get(req, res, Users);
 
-export const update = async (req, res) => {
-  const { id } = req.params;
-  const { name } = req.body;
-
-  try {
-    const user = await Users.findById(id);
-
-    await user.update({
-      name,
-    });
-
-    const userUpdated = await Users.findById(id);
-
-    res.json(userUpdated);
-  } catch (e) {
-    console.error(e);
-    res.status(500).send(e);
-  }
-};
+export const update = (req, res) => Controller.update(req, res, Users, {
+  name: nameSelType,
+});
 
 export const accounts = async (req, res) => {
   const { id } = req.params;
