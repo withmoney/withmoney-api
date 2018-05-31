9020e6c25a67c4196cbca9df96b1a88abe454947
David Costa <davidcostadev@gmail.com>
2018-05-30 21:40:12 -0300

    Add vue-material plugin and basic structure

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,20 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <md-toolbar>
+      <h3 class="md-title">With Money</h3>
+    </md-toolbar>
+    <md-table>
+      <md-table-row>
+        <md-table-head md-numeric>ID</md-table-head>
+        <md-table-head>Nome</md-table-head>
+        <md-table-head>Valor</md-table-head>
+        <md-table-head>Pago?</md-table-head>
+        <md-table-head>Data</md-table-head>
+      </md-table-row>
+    </md-table>
+    <md-content>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit minus doloremque, animi culpa dolores explicabo vel voluptatum. Iure sed rem, veritatis maiores adipisci quam tempore? Magni perferendis ab fugiat tempora?
+    </md-content>
     <router-view/>
   </div>
 </template>
@@ -13,11 +27,9 @@ export default {
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
 }
+  .md-content {
+    padding: 16px;
+  }
 </style>
---
+++ src/plugins/vue-material.js
@@ -0,0 +1,5 @@
+import Vue from 'vue';
+import VueMaterial from 'vue-material';
+import 'vue-material/dist/vue-material.min.css';
+
+Vue.use(VueMaterial);
