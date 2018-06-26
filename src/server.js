f514b1ff1c5ab8c908c425c6e6a4f55cbc9acdee
David Costa <davidcostadev@gmail.com>
2018-06-26 17:35:48 -0300

    change port

--- src/server.js
+++ src/server.js
@@ -23,7 +23,7 @@ app.use((req, res, next) => {
 
 app.use(routes);
 
-const port = process.env.NODE_PORT || 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`server on: ${port}`);
