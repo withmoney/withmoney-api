ba7a6be44af81a0d6e2fe7dca5ea2afa020204a5
David Costa <davidcostadev@gmail.com>
2018-08-03 13:51:04 -0300

    Change sintax

--- src/migrations/20180731131336-change_user_email.js
+++ src/migrations/20180731131336-change_user_email.js
@@ -1,11 +1,11 @@
 module.exports = {
-  up: (queryInterface, Sequelize) => {
+  up: (queryInterface, Sequelize) => (
     queryInterface.changeColumn('Users', 'email', {
       type: Sequelize.STRING,
       after: 'name',
       unique: true,
-    });
-  },
+    })
+  ),
   down: (queryInterface, Sequelize) => {
     queryInterface.changeColumn('Users', 'email', {
       type: Sequelize.STRING,
