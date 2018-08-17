97dd356b3edff61415bdc35e57044ca3c111f3f1
David Costa <davidcostadev@gmail.com>
2018-08-17 10:54:39 -0300

    Add getModelAlias test

--- test/utils/model.test.js
+++ test/utils/model.test.js
@@ -1,17 +1,49 @@
 import bcrypt from 'bcrypt';
-import { cryptPassword } from '../../src/utils/model';
+import { cryptPassword, getModelAlias } from '../../src/utils/model';
 
-describe('cryptPassword should', () => {
-  it('sucessful', async () => {
-    const user = {
-      password: '123',
-      _previousDataValues: {
-        password: '1234',
-      },
-    };
+describe('model', () => {
+  describe('getModelAlias should', () => {
+    it('without alias', async () => {
+      const Model = {
+        Users: 'Users',
+      };
 
-    await cryptPassword(user);
+      const result = getModelAlias({}, Model)('Users');
 
-    expect(bcrypt.compareSync('123', user.password)).toBe(true);
+      expect(result).toEqual({
+        model: 'Users',
+      });
+    });
+
+    it('with alias', async () => {
+      const Model = {
+        Accounts: 'Accounts',
+      };
+
+      const result = getModelAlias({
+        AccountFrom: 'Accounts',
+        AccountTo: 'Accounts',
+      }, Model)('AccountFrom');
+
+      expect(result).toEqual({
+        as: 'AccountFrom',
+        model: 'Accounts',
+      });
+    });
+  });
+
+  describe('cryptPassword should', () => {
+    it('sucessful', async () => {
+      const user = {
+        password: '123',
+        _previousDataValues: {
+          password: '1234',
+        },
+      };
+
+      await cryptPassword(user);
+
+      expect(bcrypt.compareSync('123', user.password)).toBe(true);
+    });
   });
 });
