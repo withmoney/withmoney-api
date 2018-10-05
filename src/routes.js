ea4b2b3d37c8de44f1461b40495096470d3c8602
David Costa <davidcostadev@gmail.com>
2018-10-05 14:51:23 -0300

    Refacting the router

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,12 @@ import { Router } from 'express';
 import { Users as UsersModel } from './models';
 import { injectModel } from './services/inject';
 import { middleware } from './middleware';
-import { resourcesAuth } from './services/resources';
+import {
+  resourceList,
+  resourceWithAuth,
+  namespace,
+  namespaceIndexCreator,
+} from './utils/routers';
 import Auth from './controllers/Auth';
 import Users from './controllers/Users';
 import Accounts from './controllers/Accounts';
@@ -13,92 +18,36 @@ import Journals from './controllers/Journals';
 const router = Router();
 
 router.get('/', (req, res) => {
-  res.send({
-    apis: {
-      v1: {
-        users: [
-          '[get] /api/v1/users',
-          '[post] /api/v1/users',
-          '[get] /api/v1/users/:id',
-          '[delete] /api/v1/users/:id',
-          '[put] /api/v1/users/:id',
-          '[get] /api/v1/users/:id/accounts',
-        ],
-        accounts: [
-          '[get] /api/v1/accounts',
-          '[post] /api/v1/accounts',
-          '[get] /api/v1/accounts/:id',
-          '[delete] /api/v1/accounts/:id',
-          '[put] /api/v1/accounts/:id',
-        ],
-        categories: [
-          '[get] /api/v1/categories',
-          '[post] /api/v1/categories',
-          '[get] /api/v1/categories/:id',
-          '[delete] /api/v1/categories/:id',
-          '[put] /api/v1/categories/:id',
-        ],
-        transactions: [
-          '[get] /api/v1/transactions',
-          '[post] /api/v1/transactions',
-          '[get] /api/v1/transactions/:id',
-          '[delete] /api/v1/transactions/:id',
-          '[put] /api/v1/transactions/:id',
-        ],
-        journals: [
-          '[get] /api/v1/journals',
-          '[post] /api/v1/journals',
-          '[get] /api/v1/journals/:id',
-          '[delete] /api/v1/journals/:id',
-          '[put] /api/v1/journals/:id',
-        ],
-        auth: [
-          '[post] /api/v1/login',
-        ],
-      },
-    },
-  });
+  res.send(namespaceIndexCreator({
+    users: resourceList('users', [
+      '[get] /api/v1/users/:id/accounts',
+    ]),
+    accounts: resourceList('accounts'),
+    categories: resourceList('categories'),
+    transactions: resourceList('transactions'),
+    journals: resourceList('journals'),
+    auth: [
+      '[post] /api/v1/login',
+    ],
+  }));
 });
 
-const namespace = '/api/v1/';
+const options = { router, middleware };
 
-// resourcesAuth
-resourcesAuth(`${namespace}accounts`, {
-  router,
-  controller: Accounts,
-  middleware,
-});
-
-resourcesAuth(`${namespace}categories`, {
-  router,
-  controller: Categories,
-  middleware,
-});
-
-resourcesAuth(`${namespace}users`, {
-  router,
-  controller: Users,
-  middleware,
-});
-resourcesAuth(`${namespace}transactions`, {
-  router,
-  controller: Transactions,
-  middleware,
-});
-resourcesAuth(`${namespace}journals`, {
-  router,
-  controller: Journals,
-  middleware,
-});
+resourceWithAuth('accounts', Accounts, options);
+resourceWithAuth('categories', Categories, options);
+resourceWithAuth('users', Users, options);
+resourceWithAuth('transactions', Transactions, options);
+resourceWithAuth('journals', Journals, options);
 
 // custom routers
 
 router.get(
-  `${namespace}users/:id/accounts`,
+  namespace('users/:id/accounts'),
   middleware,
   Users.accounts(injectModel(UsersModel)),
 );
 
-router.post(`${namespace}login`, Auth.login);
+router.post(namespace('login'), Auth.login);
 
 export default router;
