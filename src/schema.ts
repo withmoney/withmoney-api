2b7caf41f7b0d54b9d30757300230520e5803410
David Costa <davidcostadev@gmail.com>
2020-11-23 22:17:13 -0300

    Add artifact

--- src/schema.ts
+++ src/schema.ts
@@ -34,7 +34,16 @@ import { nexusSchemaPrisma } from 'nexus-plugin-prisma/schema';
 
 export const schema = makeSchema({
   types,
-  plugins: [nexusSchemaPrisma()],
+  plugins: [
+    // nexusSchemaPrisma(),
+    nexusSchemaPrisma({
+      // Generate typefiles on any occasions
+       shouldGenerateArtifacts : true,
+       outputs : {
+         typegen: __dirname + '/generated/typegen-nexus-plugin-prisma.d.ts'
+       }
+     })
+  ],
   outputs: {
     schema: path.join(__dirname, './../schema.graphql'),
     typegen: path.join(__dirname, './generated/nexus.ts'),
