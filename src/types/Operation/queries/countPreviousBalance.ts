44cafef967d98cb157a5ae10163f1664458e4e82
David Costa <davidcostadev@gmail.com>
2021-02-12 18:01:47 -0300

    Fix the count previous balance query

--- src/types/Operation/queries/countPreviousBalance.ts
+++ src/types/Operation/queries/countPreviousBalance.ts
@@ -24,6 +24,7 @@ export const CountPreviousBalanceQuery = queryField('countPreviousBalance', {
       where: {
         ...args.where,
         isPaid: true,
+        deletedAt: null,
         userId,
       },
     });
